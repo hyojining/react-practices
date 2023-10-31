15bdf5ef89c798038423affe247c68be675169b0
hyojining <102501038+hyojining@users.noreply.github.com>
2023-10-31 16:58:16 +0900

    component #2

--- 1.basics/ex08/src/01/App.js
+++ 1.basics/ex08/src/01/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-function App() {    
-    // const App = React.createElement('div', null, 'Hello World');
-    // return App;
+function App() {
     return (
         <div>
             <h1>01</h1>
--- 2.component/readme.txt
+++ 2.component/readme.txt
@@ -3,7 +3,6 @@ React Practices - Component
 ex01: property
     1. Component 작성법
     2. Data Flow: Top -> Down
-
 ex02: Component Styling
 ex03: React Event
 ex04: State
