82e366cf9f6d0ca40758b0007489cb2e67df42a0
hyojining <102501038+hyojining@users.noreply.github.com>
2023-11-03 09:56:17 +0900

    #comments

--- 2.component/ex03/src/06/App.js
+++ 2.component/ex03/src/06/App.js
@@ -6,18 +6,21 @@ export default class App extends Component {
         super(props);
     }
 
+    // 스크롤 이벤트 핸들러
     onScroll() {
+        // this는 여기에서 정의되지 않았으므로 메서드를 호출한 컨텍스트(주로 이벤트 핸들러 자체)에 바인딩 되어야 한다.
         console.log(this.outterRef.scrollTop, this.outterRef.clientHeight, this.innerRef.clientHeight);      
     }
 
     render() {
         return (
             <div
-                ref={ (ref) => this.outterRef = ref }
+                ref={ (ref) => this.outterRef = ref } // 외부 div 요소에 ref 설정
                 className={'App'}
                 onScroll={this.onScroll.bind(this)}>
                 <div
-                    ref={ (ref) => this.innerRef = ref }>
+                    ref={ (ref) => this.innerRef = ref } // 내부 div 요소에 ref 설정
+                >
                     <ul>
                         {Array.from({length: 100}, (_, i) => i + 1).map(i =>
                             <li key={i}>
