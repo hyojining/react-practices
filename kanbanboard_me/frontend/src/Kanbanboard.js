c5cb89f4a8e7de7258cc6e93ff2dea3fba8c60bd
hyojining <102501038+hyojining@users.noreply.github.com>
2023-11-05 18:00:50 +0900

    kanbanboard #1

--- kanbanboard_me/frontend/src/Kanbanboard.js
+++ kanbanboard_me/frontend/src/Kanbanboard.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import styles from './assets/css/Kanbanboard.css';
 import data from './assets/json/data';
+import CardList from './CardList';
 
 function Kanbanboard(props) {
-    const cardsTodo = data.filter(card => card.status === 'Todo');
-    console.log(cardsTodo);
+    const cardsTodo = data.filter(card => card.status === 'ToDo');
+    const cardsDoing = data.filter(card => card.status === 'Doing');
+    const cardsDone = data.filter(card => card.status === 'Done');
 
     return (
-        <div>
-            Kanbanboard
+        <div className={styles.KanbanBoard}>
+            <CardList title={'To do'} cards = {cardsTodo} />
+            <CardList title={'Doing'} cards = {cardsDoing} />
+            <CardList title={'Done'} cards = {cardsDone} />
         </div>
     );
 }
---
+++ kanbanboard_me/frontend/src/TaskList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Task from './Task';
+
+function TaskList({tasks}) {
+    return (
+        <div class='TaskList'>
+            <ul>
+            {
+                tasks.map(task => <Task 
+                                        key={task.no}
+                                        name={task.name} 
+                                        done={task.done}/>)
+            }
+            </ul>
+        </div>
+    );
+}
+
+export default TaskList;
