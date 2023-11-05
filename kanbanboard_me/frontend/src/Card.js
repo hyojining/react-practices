2883561329df8b855f29877969997639dc088c35
hyojining <102501038+hyojining@users.noreply.github.com>
2023-11-05 18:38:52 +0900

    kanbanboard #useState

--- kanbanboard_me/frontend/src/Card.js
+++ kanbanboard_me/frontend/src/Card.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './assets/css/Card.css'
 import TaskList from './TaskList';
 
 function Card({title, description, tasks}) {
+    const [details, setDetails] = useState(false);
     return (
         <div className={styles.Card}>
-            <div className={styles.Card__Title}>{title}</div>
-            <div className={styles.Card__Details}>
-                {description}
-                <TaskList tasks={tasks}/>
-            </div>
+            <div className={`${styles.Card__Title} ${details ? styles.Card__Title__open : ''}`}
+                onClick={() => setDetails(!details)}>{title}</div>
+
+            {details ? 
+                <div className={styles.Card__Details} >
+                    {description}
+                    <TaskList tasks={tasks}/>
+                </div> :
+                null
+            }
         </div>
     );
 }
--- kanbanboard_me/frontend/src/Task.js
+++ kanbanboard_me/frontend/src/Task.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './assets/css/Task.css';
 
 function Task({name, done}) {
+    const [isdone, setDone] = useState(done);
+
     return (
         <li className={styles.TaskList__Task}>
-            <input type='checkbox' checked={done} />
+            <input type='checkbox' checked={isdone} onChange={() => setDone(!isdone)}/>
             {name}
             <a href='#' className={styles.TaskList__Task__remove}></a>
         </li>
