759695fe459fd45d8baf45c17c541c6d143234e6
hyojining <102501038+hyojining@users.noreply.github.com>
2023-11-06 19:30:18 +0900

    emaillist #submit

--- emaillist/backend/src/main/java/com/poscodx/emaillist/controller/ApiController.java
+++ emaillist/backend/src/main/java/com/poscodx/emaillist/controller/ApiController.java
@@ -1,18 +1,36 @@
 package com.poscodx.emaillist.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.poscodx.emaillist.dto.JsonResult;
+import com.poscodx.emaillist.repository.EmailllistRepository;
+import com.poscodx.emaillist.vo.EmaillistVo;
 
 @RestController
 public class ApiController {
+	@Autowired
+	private EmailllistRepository emaillistRepository;
+	
 	@GetMapping("/api")
 	public ResponseEntity<JsonResult> read() {
 		return ResponseEntity
 				.status(HttpStatus.OK)
-				.body(JsonResult.success("hello world"));
+				.body(JsonResult.success(emaillistRepository.findAll()));
+	}
+	
+	@PostMapping("/api")
+	public ResponseEntity<JsonResult> create(@RequestBody EmaillistVo vo) {		
+		Long no = emaillistRepository.insert(vo);
+		vo.setNo(no);
+		
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(JsonResult.success(vo));
 	}
 }
--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,12 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './assets/scss/App.scss';
 import RegisterForm from './RegisterForm';
 import SearchBar from './SearchBar';
 import Emaillist from './Emaillist';
-import data from './assets/json/data';
+// import data from './assets/json/data';
 
 function App() {
-    const [emails, setEmails] = useState(data);
+    const [emails, setEmails] = useState(null);
     const searchEmail  = (keyword) => {
         // 배열(data)의 각 요소에 대해 필터링 함수(email => ...)를 호출하며, 배열의 각 요소가 email 변수로 전달된다.
         // filter 메소드는 배열의 각 요소에 대해 주어진 조건을 검사하고 조건을 만족하는 요소만 남긴 새로운 배열을 반환한다.
@@ -14,11 +14,77 @@ function App() {
         setEmails(newEmails);
     };
 
+    const addEmail = async (email) => {
+        try{
+            const insertResponse = await fetch('/api', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(email)
+            });
+
+            if(!insertResponse.ok) {
+                throw new Error(`${insertResponse.status} ${insertResponse.statusText}`)
+            }
+
+            const json = await insertResponse.json();
+            
+            if(json.result !== 'success') {
+                throw new Error(`${json.result} ${json.message}`)
+            }
+
+            const newEmails = [...emails, json.data];
+            setEmails(newEmails);
+            
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    const fetchList = async() => {
+        try{
+            const response = await fetch('/api', {
+                method: 'get',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: null
+            });
+
+            if(!response.ok) {
+                throw new Error(`${response.status} ${response.statusText}`)
+            }
+
+            const json = await response.json();
+            
+            if(json.result !== 'success') {
+                throw new Error(`${json.result} ${json.message}`)
+            }
+
+            console.log(json.data);
+            setEmails(json.data);
+            
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    useEffect(() => {
+        fetchList();
+    }, []);
+
     return (
         <div id={'App'}>
-            <RegisterForm />
+            <RegisterForm addEmail={addEmail}/>
             <SearchBar searchEmail={searchEmail}/>
-            <Emaillist emails={emails}/>
+            {
+                emails === null ?
+                null :
+                <Emaillist emails={emails}/>
+            }
         </div>
     );
 }
