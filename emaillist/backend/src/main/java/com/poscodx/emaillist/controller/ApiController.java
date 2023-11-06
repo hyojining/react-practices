51e061fbbc85f80550acde4317c3c309b4c78acd
hyojining <102501038+hyojining@users.noreply.github.com>
2023-11-06 15:48:46 +0900

    emaillist api #3

--- emaillist/backend/src/main/java/com/poscodx/emaillist/controller/ApiController.java
+++ emaillist/backend/src/main/java/com/poscodx/emaillist/controller/ApiController.java
@@ -1,5 +1,7 @@
 package com.poscodx.emaillist.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,9 @@ import com.poscodx.emaillist.dto.JsonResult;
 @RestController
 public class ApiController {
 	@GetMapping("/api")
-	public JsonResult read() {
-		return JsonResult.success("hello world");
+	public ResponseEntity<JsonResult> read() {
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(JsonResult.success("hello world"));
 	}
 }
