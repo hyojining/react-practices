427e4245ff88306c683c228a09c5257480fb9068
hyojining <102501038+hyojining@users.noreply.github.com>
2023-11-07 10:28:18 +0900

    emaillist #final

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -7,12 +7,6 @@ import Emaillist from './Emaillist';
 
 function App() {
     const [emails, setEmails] = useState(null);
-    const searchEmail  = (keyword) => {
-        // 배열(data)의 각 요소에 대해 필터링 함수(email => ...)를 호출하며, 배열의 각 요소가 email 변수로 전달된다.
-        // filter 메소드는 배열의 각 요소에 대해 주어진 조건을 검사하고 조건을 만족하는 요소만 남긴 새로운 배열을 반환한다.
-        const newEmails = data.filter(email => email.firstName.indexOf(keyword) !== -1 || email.lastName.indexOf(keyword) !== -1 || email.email.indexOf(keyword) !== -1);
-        setEmails(newEmails);
-    };
 
     const addEmail = async (email) => { // 비동기 함수
         try{
@@ -43,9 +37,9 @@ function App() {
         }
     }
 
-    const fetchList = async() => {
+    const fetchEmails = async(keyword) => {
         try{
-            const response = await fetch('/api', {
+            const response = await fetch(`/api?kw=${keyword ? keyword : ''}`, {
                 method: 'get',
                 headers: {
                     'Content-Type': 'application/json',
@@ -73,13 +67,13 @@ function App() {
     }
 
     useEffect(() => {
-        fetchList();
+        fetchEmails();
     }, []);
 
     return (
         <div id={'App'}>
             <RegisterForm addEmail={addEmail}/>
-            <SearchBar searchEmail={searchEmail}/>
+            <SearchBar fetchEmails={fetchEmails}/>
             {
                 emails === null ?
                 null :
--- emaillist/frontend/src/SearchBar.js
+++ emaillist/frontend/src/SearchBar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './assets/scss/SearchBar.scss';
 
-function SearchBar({searchEmail}) {
+function SearchBar({fetchEmails}) {
     return (
         <div className={styles.Searchbar}>
             <input 
                 type='text' 
                 placeholder='찾기'
-                onChange={e => { //검색어가 변경될 때마다 searchEmail 함수를 호출하여 검색 기능을 수행
-                    searchEmail(e.target.value);
+                onChange={e => {
+                    fetchEmails(e.target.value);
                 }}/>
         </div>
     );
