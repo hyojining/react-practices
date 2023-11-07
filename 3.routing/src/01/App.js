d48be64a31a8e8d5baeca45d8ecfa86f23c5316b
hyojining <102501038+hyojining@users.noreply.github.com>
2023-11-07 17:45:26 +0900

    route #comments

--- 3.routing/src/01/App.js
+++ 3.routing/src/01/App.js
@@ -6,8 +6,10 @@ import Guestbook from "./component/Guestbook";
 export default function App() {
     const [route, setRoute] = useState("/");
     
-    useEffect(() => {
+    useEffect(() => { 
+        // 컴포넌트가 마운트될 때 handlerHashChange함수를 해시 변경 이벤트에 등록
         window.addEventListener('hashchange', handlerHashChange);
+        
         return () => {
             window.removeEventListener('hashchange', handlerHashChange);
         }
@@ -15,10 +17,12 @@ export default function App() {
 
     const handlerHashChange = () => {
         console.log(window.location.hash);
+
+        // 현재 URL의 해시 부분에서 첫 번째 문자인 '#' 제거하여 실제 경로를 추출
         setRoute(window.location.hash.substring(1));
     }
 
-    return (() => {
+    return (() => { // route 값에 따라 컴포넌트 렌더링
         switch(route) {
             case '/' :
                 return <Main />;
--- 3.routing/src/02/App.js
+++ 3.routing/src/02/App.js
@@ -4,26 +4,26 @@ import Gallery from "./component/Gallery";
 import Guestbook from "./component/Guestbook";
 
 export default function App() {
-    const [route, setRoute] = useState({page: window.location.href.substring(window.location.href.lastIndexOf("/"))});
-
+    const [route, setRoute] = useState({page: window.location.href.substring(window.location.href.lastIndexOf("/"))}); // 현재 URL에서 마지막 '/' 문자 이후의 문자열
     useEffect(() => {
         const handlePopState = () => {
-            setRoute(e.state);
+            setRoute(e.state); // route 상태 업데이트
         };
-
-        window.addEventListener('popstate', handlePopState);
+        window.addEventListener('popstate', handlePopState); // 브라우저의 뒤 or 앞으로 가기 버튼 클릭 시, 발생하는 이벤트
+        
         return () => {
             window.removeEventListener('popstate', handlePopState);
         }
     }, []);
 
+    // 페이지 내의 링트가 클릭될 때, 호출되는 함수
     const handleLinkClick = (e) => {
-        e.preventDefault();
+        e.preventDefault(); // 페이지 이동 막기
 
-        const url = e.target.href.substring(e.target.href.lastIndexOf("/"));
+        const url = e.target.href.substring(e.target.href.lastIndexOf("/")); // 클릭된 링크의 URL ('/'문자 이후의 문자열)
         console.log(url);
-        window.history.pushState({page: url}, e.target.text, url);
-        setRoute({page: url});
+        window.history.pushState({page: url}, e.target.text, url); // 웹 브라우저의 히스토리 스택에 새로운 항목을 추가
+        setRoute({page: url}); // 클릭된 링크로 route 상태 업데이트
     }
 
     const router = function () {
